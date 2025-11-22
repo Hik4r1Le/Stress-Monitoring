6fef87396a12f5374040c9ebbec61e9caa6ec0e7
Lê Vĩnh Huy <150580083+HikariL3@users.noreply.github.com>
2025-11-22 10:27:05 +0700

    change server.js for postgresql

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ app.use(express.json());
 
 // ------------------ DATABASE ------------------
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    host:     process.env.DB_HOST,
+    user:     process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port:     process.env.DB_PORT || 3306,
+    ssl:      process.env.DB_SSL ? { rejectUnauthorized: false } : null,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
